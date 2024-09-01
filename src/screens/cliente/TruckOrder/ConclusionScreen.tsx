e4121f8adcdb1d0e38a36d154381367d716f15d1
DDark <antoniolexis2001@outlook.com>
2024-09-02 00:05:45 +0100

    ajustes em tela de solicitacao terminada!

--- src/screens/cliente/TruckOrder/ConclusionScreen.tsx
+++ src/screens/cliente/TruckOrder/ConclusionScreen.tsx
@@ -2,17 +2,39 @@
 
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {Routes} from 'app/constants/enums';
+import Layout from 'app/styles/Layout';
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
-
-import {Button, Text} from 'react-native-paper';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {Button} from 'react-native-paper';
 
 const ConclusionScreen: React.FC<
   NativeStackScreenProps<StackScreen, Routes.CLIENT_FINISHED_ORDER>
 > = ({navigation, route}): React.JSX.Element => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Thank you for placing your order!</Text>
+      <View
+        style={{
+          width: 150,
+          height: 150,
+          borderRadius: 100,
+          backgroundColor: '#ccc',
+          justifyContent: 'center',
+          alignItems: 'center',
+          bottom: 20,
+        }}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={require('app/assets/images/icon.png')}
+        />
+      </View>
+      <Text style={styles.text}>Valor a pagar</Text>
+      <Text style={{fontSize: 50, fontWeight: 'normal'}}>R$ 0,00</Text>
+      <Button
+        onPress={() => navigation.navigate(Routes.CLIENT_HOME)}
+        style={{marginTop: 20, borderRadius: Layout.radius}}
+        mode="contained">
+        Terminar
+      </Button>
     </View>
   );
 };
@@ -23,24 +45,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  
-  image: {
-    width: 120,
-    height: 120,
-    marginBottom: 20,
-  },
   text: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  value: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  payment: {
-    fontSize: 20,
-    marginBottom: 20,
   },
 });
 
